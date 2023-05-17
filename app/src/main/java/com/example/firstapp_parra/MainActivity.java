a9897c425b9bfe3a8ff18f1916a3e07a7850c54c
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-05-17 14:30:46 -0500

    Updated main activity file with a new method

--- app/src/main/java/com/example/firstapp_parra/MainActivity.java
+++ app/src/main/java/com/example/firstapp_parra/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        modification();
+    }
+
+    private void modification(){
+        int a = 23;
     }
 }
